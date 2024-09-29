6f919bd0739f45b4d33787c4eda8d61e812a0402
Minh Vương <hardingadonis@gmail.com>
2024-09-29 08:52:27 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173, 
-  },
-  build: {
-    outDir: 'dist',     
-    emptyOutDir: true  
-  }
+  base: '/'
 })
